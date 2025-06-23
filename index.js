d0f3ef5d8f212c962a62a0e5e2563daa3cbcadda
Diogo Mascarenhas <diogomascarenhas0574@gmail.com>
2025-06-23 16:53:00 -0300

    add validarEntradaNoEvento

---
+++ index.js
@@ -0,0 +1,10 @@
+var nome = Diogo;
+var idade = 23;
+
+function validarEntradaNoEvento() {
+  if (idade >= 18) {
+    console.log("Pode entrar no evento");
+  } else {
+    console.log("Nao pode entrar no evento");
+  }
+}
