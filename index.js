eff6129497ed60427524c2c6b887915171f72fc0
Diogo Mascarenhas <diogomascarenhas0574@gmail.com>
2025-06-23 16:54:50 -0300

    atualizacao boa

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var nome = Diogo;
 var idade = 23;
 
 function validarEntradaNoEvento() {
-  if (idade >= 18) {
-    console.log("Pode entrar no evento");
-  } else {
+  if (idade <= 18) {
     console.log("Nao pode entrar no evento");
+  } else {
+    console.log("Pode entrar no evento");
   }
 }
